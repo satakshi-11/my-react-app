1aa5775f35e81e1273296e272f7fefb3f946a040
Km Satakshi Singh <105787853+satakshi-11@users.noreply.github.com>
2024-03-20 16:16:02 +0530

    17 vdo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { useState } from 'react';
+import Alert from './components/Alert';
+{/*import About from './components/About';*/}
+
+{/*import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';*/}
 function App() {
+  const [mode,setMode]=useState('light');
+
+  const [alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
+ 
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#000435';
+      showAlert(' Dark mode has been enabled','success');
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert(' Light mode has been enabled',' success');
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Navbar title="Textutils" about="About Textutils" mode={mode}  toggleMode={toggleMode}/>
+     <Alert alert={alert}/>
+    <div className="container my-3">
+	  {/* <Routes>
+		 <Route exact path='/about' element={<About/>}></Route>*/}
+     {/*<Route exact path='/' element=*/}
+     <TextForm showAlert={showAlert} mode={mode}heading="Enter the text to analyse below"/> {/*></Route>         
+     </Routes>*/}
+     </div>
+     
+      </>
+  )
+
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+
+export default function TextForm(props) {
+  // eslint-disable-next-line
+  const handleLowClick=()=>{
+    
+    let newtext=text.toLowerCase();
+    setText(newtext);
+    // eslint-disable-next-line
+   { props.showAlert(' Converted to Lowercase','success')};
+  }
+  // eslint-disable-next-line
+  const clear=()=>{
+    let newtext="";
+    setText(newtext);
+    // eslint-disable-next-line
+    { props.showAlert(' Cleared workspace','success')}; 
+  }
+  // eslint-disable-next-line
+  const removespace=()=>{
+    let newtext=text.replace(/\s+/g," ");
+    setText(newtext);
+    // eslint-disable-next-line
+    { props.showAlert(' Removed spaces','success')};
+  }
+
+    const handleUpClick=()=>{
+    
+        let newtext=text.toUpperCase();
+        setText(newtext);
+        // eslint-disable-next-line
+        { props.showAlert(' Converted to Uppercase','success')};
+    }
+    const handleonchange=(event)=>{
+       
+        setText(event.target.value)
+    }
+    const [text,setText]=useState('Enter Text Here')
+  return (
+    <>
+<div style={{color: props.mode==='dark'?'white':'black'}}>
+    <h1>{props.heading}</h1>
+   <div className="mb-3">
+    <textarea className="form-control" value={text} onChange={handleonchange} style={{backgroundColor:props.mode==='dark'?'#000435':'white', color: props.mode==='dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+       </div>
+       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+       <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+       <button className="btn btn-primary mx-2" onClick={clear}>Clear Text</button>
+       <button className="btn btn-primary mx-2" onClick={removespace}>Remove Extra Spaces</button>
+</div>
+   <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
+    <h2>Text Summary</h2>
+    <p>{text.split(" ").length-1} words and {text.length} characters</p>
+    <p>{0.008*text.split(" ").length} Minutes to read</p>
+    <h3>Preview</h3>
+    <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+   </div>
+</>
+  )
+}
