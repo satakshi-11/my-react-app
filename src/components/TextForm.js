8b8c8db5c0895100b120b93bbaadf2f08853bf52
Km Satakshi Singh <105787853+satakshi-11@users.noreply.github.com>
2024-03-22 19:16:44 +0530

    different modes on navbar

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,8 +51,8 @@ export default function TextForm(props) {
 </div>
    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
     <h2>Text Summary</h2>
-    <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-    <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to read</p>
+    <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+    <p>{0.008*text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes to read</p>
     <h3>Preview</h3>
     <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
    </div>
